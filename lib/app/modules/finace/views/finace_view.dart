c0c367f39b17dee66bb76760d14085dffc9c073c
tagirahmad <tagirbpl@gmail.com>
2021-03-28 10:54:07 +0300

    Update finance view

--- lib/app/modules/finace/views/finace_view.dart
+++ lib/app/modules/finace/views/finace_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:l_l_app/app/constants/colors.dart';
 import 'package:l_l_app/app/constants/dimensions.dart';
+import 'package:l_l_app/app/widgets/card_button.dart';
+import 'package:l_l_app/app/widgets/finance_popup.dart';
 import 'package:l_l_app/app/widgets/round_button.dart';
 
 import '../controllers/finace_controller.dart';
@@ -35,14 +37,56 @@ class FinanceView extends GetView<FinaceController> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    // CardButton(
-                    //   color: AppColors.LIGHT_BLUE,
-                    //   onPress: () {},
-                    // ),
                     CardButton(
                       color: AppColors.LIGHT_GREEN,
                       title: 'Доходы',
-                      onPress: () {},
+                      onPress: () {
+                        showDialog<Widget>(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return FinancePopup(
+                                title: 'Доходы',
+                                backgroundColor: AppColors.LIGHT_GREEN,
+                                content: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    TextField(
+                                      autofocus: true,
+                                      style:
+                                          const TextStyle(color: Colors.white),
+                                      keyboardType: TextInputType.number,
+                                      decoration: const InputDecoration(
+                                          labelText: 'Доход мужа',
+                                          hintText: 'Доход мужа / мес',
+                                          hintStyle:
+                                              TextStyle(color: Colors.white),
+                                          labelStyle:
+                                              TextStyle(color: Colors.white)),
+                                      onChanged: (String text) {
+                                        print(text);
+                                      },
+                                    ),
+                                    TextField(
+                                      keyboardType: TextInputType.number,
+                                      style:
+                                          const TextStyle(color: Colors.white),
+                                      decoration: const InputDecoration(
+                                          labelText: 'Доход жены',
+                                          hintText: 'Доход жены / мес',
+                                          hintStyle:
+                                              TextStyle(color: Colors.white),
+                                          labelStyle:
+                                              TextStyle(color: Colors.white)),
+                                      onChanged: (String text) {
+                                        print(text);
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              );
+                            });
+                      },
                     ),
                     CardButton(
                       color: AppColors.LIGHT_YELLOW,
@@ -85,71 +129,5 @@ class FinanceView extends GetView<FinaceController> {
                 )
               ]),
             )));
-  }
-}
-
-class CardButton extends StatelessWidget {
-  const CardButton(
-      {Key? key,
-      required this.color,
-      required this.onPress,
-      required this.title})
-      : super(key: key);
-
-  final Color color;
-  final Function() onPress;
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      clipBehavior: Clip.none,
-      alignment: AlignmentDirectional.center,
-      children: <Widget>[
-        Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Container(
-                width: MediaQuery.of(context).size.width * .4,
-                decoration: BoxDecoration(
-                    color: color.withOpacity(0.6),
-                    borderRadius:
-                        BorderRadius.circular(Dimensions.BORDER_RADIUS)),
-                padding: const EdgeInsets.all(Dimensions.SIDE_INDENT),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      title,
-                      style: Theme.of(context)
-                          .textTheme
-                          .subtitle1!
-                          .copyWith(color: Colors.white),
-                    ),
-                    const SizedBox(height: Dimensions.SIDE_INDENT),
-                    Text(
-                      '        .руб',
-                      style: Theme.of(context)
-                          .textTheme
-                          .subtitle1!
-                          .copyWith(color: Colors.white),
-                    ),
-                    const SizedBox(height: Dimensions.SIDE_INDENT),
-                  ],
-                )),
-            const SizedBox(height: 25)
-          ],
-        ),
-        Positioned(
-          // bottom: -25,
-          bottom: 0,
-          child: RoundButton(
-              onPressed: onPress,
-              hasBorder: true,
-              iconPath: 'assets/images/dots.svg',
-              color: color),
-        )
-      ],
-    );
   }
 }
